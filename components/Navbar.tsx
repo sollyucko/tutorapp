604d1334b0dcba118a96f449c60e4d55b0220529
Linkai Wu <linkai@linkaiwu.com>
2022-03-11 09:54:32 -0500

    Initial commit + navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export default function Navbar(props) {
+  const { ...rest } = props;
+
+  return (
+    <Guest {...rest}/>
+  );
+}
+
+const Guest = (props) => {
+  const { ...rest } = props;
+
+  return (
+    <nav {...rest}>
+      <div className="flex-none flex items-center">
+        <a className="cursor-pointer">
+          <h1 className="text-3xl font-bold">tutorapp</h1>
+        </a>
+      </div>
+      <div className="flex-auto flex items-center justify-end gap-6">
+        <a className="flex-none cursor-pointer hover:underline underline-offset-1 decoration-2 decoration-blue-500">
+          Explore
+        </a>
+
+        <button className="px-3 py-2 rounded-lg bg-blue-500 shadow-md shadow-blue-500 font-semibold text-white">
+          Login/Register
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+const Authenticated = (props) => {
+  const { ...rest } = props;
+
+  return (
+    <nav {...rest}>
+      <div className="flex-none flex items-center">
+        <a className="cursor-pointer">
+          <h1 className="text-3xl font-bold">tutorapp</h1>
+        </a>
+      </div>
+      <div className="flex-auto flex items-center justify-end gap-6">
+        <a className="cursor-pointer hover:underline underline-offset-1 decoration-2 decoration-blue-500">
+          Dashboard
+        </a>
+        <a className="cursor-pointer hover:underline underline-offset-1 decoration-2 decoration-blue-500">
+          Explore
+        </a>
+
+        <button className="">
+          <img className="h-10 w-10 rounded-full ring-2 ring-white shadow-md shadow-gray-600" src="https://loremflickr.com/256/256" alt="Profile"/>
+        </button>
+      </div>
+    </nav>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  return <>
+    <Head>
+      <title>tutorapp</title>
+      <meta name="description" content="by @linkai101 on github" />
+    </Head>
+
+    <Navbar className="container flex px-4 py-4"/>
+
+    <main>
+      <div className="h-80 bg-blue-200 flex items-center justify-center">
+        <h2 className="text-4xl font-semibold text-blue-500 underline decoration-4 decoration-blue-600">
+          insert motto here.
+        </h2>
+      </div>
+    </main>
+
+    <footer>
+    </footer>
+  </>;
+}

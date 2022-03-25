7d8d757861fe64299698fa46a200c221387fe090
Linkai Wu <linkai@linkaiwu.com>
2022-03-25 17:52:17 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  assetPrefix: 'https://code.linkaiwu.com/proxy/3000', // DEV ONLY
+  //assetPrefix: 'https://code.linkaiwu.com/proxy/3000', // DEV ONLY
   reactStrictMode: true,
 }
